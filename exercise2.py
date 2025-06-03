6700c1f1b65847d5d1c2f7aa3ae648ede8070a4d
Parhom800 <pzosich@bk.ru>
2025-06-03 15:45:39 +0300

    Update exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -1,28 +1,27 @@
 from multiprocessing import Process, Pipe
 import random
 
-# Генерация чисел
+# Генерация 
 def generate(conn):
-    count = random.randint(1, 1000)  # случайное количество чисел от 1 до 1000
-    numbers = [random.randint(-1_000_000, 1_000_000) for _ in range(count)]
-    conn.send(numbers)  # передаём список через Pipe
+    count = random.randint(1, 1000)  
+    numbers = [random.randint(-1000000, 1000000) for _ in range(count)]
+    conn.send(numbers)  
     conn.close()
-    print(f"[p1] Сгенерировано {count} чисел.")
+    print(f" process 1 generated {count} numbers")
 
 # Возведение
 def square(conn):
     numbers = conn.recv()
     squared = [x**2 for x in numbers]
-    print(f"[p2] Все квадраты:\n{squared}")
+    print(f"procces 2: \n{squared}")
     conn.close()
 
+if __name__ == "__main__":
     read, write = Pipe()
 
-    # Создание процессов
     p1 = Process(target=generate, args=(write,))
     p2 = Process(target=square, args=(read,))
 
-    # Запуск процессов
     p1.start()
     p2.start()
 
